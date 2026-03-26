6e669e1230d52559f33937b40c9570fdaa88d559
Johan Suarez <johan_leo1d@outlook.com>
2026-03-26 19:43:31 -0400

    agrego ejercicio par o impar

---
+++ ejercicios.py/ejercicio1.py
@@ -0,0 +1,12 @@
+try:
+    numero = int(input("ingresa un numero: "))
+    if numero % 2 == 0:
+     print("el numero es par")
+    else:
+        print("el numero es impar")
+        
+except:
+    print("No es un dato numerico")
+               
+    
+    
